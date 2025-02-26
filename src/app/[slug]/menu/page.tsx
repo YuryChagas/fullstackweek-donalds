60a0fd1dcdb70df11a60e909d80695a749046381
YuryChagas <yury.chagas16@gmail.com>
2025-02-26 20:36:43 -0300

    feat: add reastaurant menu page with categories and products

--- src/app/[slug]/menu/page.tsx
+++ src/app/[slug]/menu/page.tsx
@@ -4,6 +4,7 @@ import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 import RestaurantHeader from "./components/header";
+import RestaurantCategories from "./components/categories";
 
 interface RestaurantMenuPageProps {
   params: Promise<{ slug: string }>;
@@ -22,13 +23,18 @@ const RestaurantMenuPage = async ({
   if (!isConsumptionMethod(consumptionMethod)) {
     return notFound();
   }
-  const restaurant = await db.restaurant.findUnique({ where: { slug: slug } });
+  const restaurant = await db.restaurant.findUnique({ where: { slug: slug }, include: {
+    menuCategories: {
+      include: { products: true}
+    }
+  } },);
   if (!restaurant) {
     return notFound();
   }
   return (
     <div>
       <RestaurantHeader restaurant={restaurant} />
+      <RestaurantCategories restaurant={restaurant}/>
     </div>
   );
 };
